0db98d2c514e3dbd7a97b0724bb29ce6ad0b455e
Monish Yeluguri <Monish Yeluguri@BetterthanU-HP>
2014-01-11 13:39:00 -0500

    Changed to Solenoid module

--- src/org/olentangyfrc/subsystems/Cylinder.java
+++ src/org/olentangyfrc/subsystems/Cylinder.java
@@ -2,7 +2,7 @@
 package org.olentangyfrc.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.Solenoid;
 import org.olentangyfrc.RobotMap;
 /**
  *
@@ -10,10 +10,10 @@ import org.olentangyfrc.RobotMap;
 public class Cylinder extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-    DigitalOutput cylinderOutput;
+    Solenoid cylinderOutput;
 public Cylinder() {
     super("Cylinder");
-    cylinderOutput = new  DigitalOutput(RobotMap.CYLINDER_OUTPUT);
+    cylinderOutput = new  Solenoid(RobotMap.CYLINDER_OUTPUT);
 }
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
