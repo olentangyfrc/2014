af92be3483507e800bec01c8478232415621daa7
Monish Yeluguri <Monish Yeluguri@BetterthanU-HP>
2014-01-11 13:05:53 -0500

    Make cylinder control subsystem and subsystem command

---
+++ src/org/olentangyfrc/subsystems/Cylinder.java
@@ -0,0 +1,33 @@
+
+package org.olentangyfrc.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.DigitalOutput;
+import org.olentangyfrc.RobotMap;
+/**
+ *
+ */
+public class Cylinder extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    DigitalOutput cylinderOutput;
+public Cylinder() {
+    super("Cylinder");
+    cylinderOutput = new  DigitalOutput(RobotMap.CYLINDER_OUTPUT);
+}
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+        
+    }
+    public void extend(){
+        cylinderOutput.set(true);
+        
+    }
+    public void release(){
+        cylinderOutput.set(false);
+        
+    }
+    
+}
+
